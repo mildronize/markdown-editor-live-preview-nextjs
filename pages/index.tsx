a031cb3a376c97b425c554f2825bb832c066db4a
Thada Wangthammang <mildronize@gmail.com>
2020-11-19 05:49:00 +0700

    add react markdown render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
-// import dynamic, { noSSR } from 'next/dynamic'
+import { useState } from 'react';
 import  MarkdownEditor from '../components/Editor/MarkdownEditor';
+import ReactMarkdown from 'react-markdown'
+import remarkGFM from 'remark-gfm';
+
 
 function IndexPage() {
+
+  const [value, setValue] = useState("");
+
+  const handleEditorChange = (newValue, e) => {
+    setValue(newValue);
+  }
 
   function editorDidMount(editor, monaco) {
     editor.focus();
@@ -9,16 +18,26 @@ function IndexPage() {
     
   return (
     <>
+    <div style={{ display: 'flex' }} >
+      <div style={{ width: '49%' }} >
       <MarkdownEditor
         height={'600px'}
         theme="vs"
-        // value={sample}
-        onChange={console.log}
         editorDidMount={editorDidMount}
+        value={value}
+        onChange={handleEditorChange}
       />
+      </div>
+      <div style={{ width: '49%' }} >
+        <h1>Output</h1>
+        <ReactMarkdown className="markdown-preview" plugins={[remarkGFM]} children={value} />
+      </div>
+    </div>
+
+      
     </>
 
   )
 }
 
-export default IndexPage
+export default IndexPage;
